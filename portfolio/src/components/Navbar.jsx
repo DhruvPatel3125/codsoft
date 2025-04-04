190eef4663c243f252b3ec78d12d5bcae87076b3
dhruv patel <dhruvjpatel5@gmail.com>
2025-04-05 00:26:05 +0530

    Enhance Navbar functionality with toggle menu and responsive styles

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -4,26 +4,35 @@ import {useState} from 'react'
 import { getImageUrl } from '../utils'  
 
 export default function Navbar() {
-  return (
-   <nav className={style.navbar}>
-        <a className={style.title} href="/">Portfolio</a>
-        <div className={style.menu}>
-            <img className={style.menuBtn} src="/assets/nav/menuIcon.png" alt="Menu Icon" />
-            <ul className={style.menuItems}>
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#experience">Experience</a>
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-   </nav>
-  )
+    const [menuOpen, setMenuOpen] = useState(false)
+return (
+ <nav className={style.navbar}>
+            <a className={style.title} href="/">Portfolio</a>
+            <div className={style.menu}>
+                    <img 
+                            className={style.menuBtn} 
+                            src={menuOpen ? "/assets/nav/closeIcon.png" : "/assets/nav/menuIcon.png"} 
+                            alt="Menu Icon"
+                            onClick={() => setMenuOpen(!menuOpen)} 
+                    />
+                    <ul 
+                            className={`${style.menuItems} ${menuOpen ? style.menuOpen : ''}`}
+                            onClick={() => setMenuOpen(false)}
+                    >
+                            <li>
+                                    <a href="#about">About</a>
+                            </li>
+                            <li>
+                                    <a href="#experience">Experience</a>
+                            </li>
+                            <li>
+                                    <a href="#projects">Projects</a>
+                            </li>
+                            <li>
+                                    <a href="#contact">Contact</a>
+                            </li>
+                    </ul>
+            </div>
+ </nav>
+)
 }
